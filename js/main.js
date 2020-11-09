106a15a6b5087feb5e77f8ff70defa72dbce4991
Sergey Protasov <s.protasov@itransition.com>
2020-11-09 16:47:01 +0300

    Improve

--- js/main.js
+++ js/main.js
@@ -10,6 +10,42 @@ const fadeOut = (el) => {
   })();
 };
 
+function fadeIn(el, display){
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+};
+
+const countTo = (el, duration = 10000) => {
+  el.started = true;
+
+  let start = null, step = null;
+  const to = parseInt(el.textContent, 10);
+  if (isNaN(to)) return;
+
+  el.textContent = 0;
+
+  (function count(timestamp) {
+    if (start) step = Math.trunc(to / (duration / (timestamp - start)));
+    if (!start) start = timestamp;
+    el.textContent = +el.textContent + (step || 1);
+
+    if (el.textContent < to) {
+      requestAnimationFrame(count);
+    } else {
+      el.textContent = to;
+    }
+  })();
+};
+
+
 const inView = (target, options, action) => {
   const interSecObs = new IntersectionObserver(entries => {
    entries.forEach(entry => {
@@ -37,7 +73,7 @@ const mapToAsocciatedObject = (nodeList, getter) => nodeListToArray(nodeList)
     {}
   );
 
-const isIntersecting = (flag, el, className) => flag
+const toggleClass = (flag, el, className) => flag
   ? el.classList.add(className)
   : el.classList.remove(className);
 
@@ -45,14 +81,21 @@ const scrollSpy = () => {
   const targets = document.querySelectorAll("header, .section"),
     top = document.querySelector("#hero-area > div .contents"),
     navBar = document.querySelector(".scrolling-navbar"),
+    backToTop = document.querySelector(".back-to-top"),
     links = mapToAsocciatedObject(targets, (id) => document.querySelector(
       `#main-navbar a.nav-link[href='#${id}']`
     ));
 
   // check if IntersectionObserver is supported
   if ("IntersectionObserver" in window) {
+    const counters = document.querySelectorAll('.counter');
+    counters.forEach(c => inView(c, {threshold: 1}, entry => {
+      entry.isIntersecting && !entry.target.started && countTo(entry.target);
+    }));
+
     inView(top, {threshold: 1}, (entry) => {
-      isIntersecting(!entry.isIntersecting, navBar, "top-nav-collapse")
+      toggleClass(!entry.isIntersecting, navBar, "top-nav-collapse")
+      toggleClass(!entry.isIntersecting, backToTop, "opacity-on")
     });
 
     targets.forEach(target => {
@@ -61,14 +104,46 @@ const scrollSpy = () => {
         if (!currentNav) {
           return;
         }
-        isIntersecting(entry.isIntersecting, currentNav, "active");
+        toggleClass(entry.isIntersecting, currentNav, "active");
       });
     });
   }
 }
 
+const setOpacity = (selector, opacity) => document.querySelectorAll(selector).forEach(el => {
+  if (opacity === 1) {
+    el.hidden = false;
+  }
+  el.style.opacity = opacity
+});
+
+const potfolio = () => {
+  const filters = document.querySelectorAll('#portfolios a.filter');
+  const all = "#portfolio > div";
+
+  document.querySelectorAll(all).forEach(c => c.addEventListener("transitionend",(event) => {
+    if (event.target.style.opacity === "0") {
+      event.target.hidden = true;
+    }
+  }));
+
+  filters.forEach(f => f.addEventListener('click', function(event) {
+    const filterValue = event.target.getAttribute('data-filter');
+    filters.forEach(f => toggleClass(f.getAttribute('data-filter') === filterValue, f,'active'))
+    if (filterValue === '*') {
+      return setOpacity(all, 1);
+    }
+    setOpacity(all + filterValue, 1);
+    setOpacity(all + `:not(${filterValue})`, 0);
+  }));
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  const loader = document.getElementById('loader');
+  const loader = document.querySelector('#loader');
+  potfolio();
+  const gallery = new Flickity('.gallery', {autoPlay:true, groupCells: true, wrapAround: true});
   scrollSpy();
   fadeOut(loader);
 });
+
+const app = Vue.createApp({});
