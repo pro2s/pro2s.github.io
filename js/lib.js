c11e6bf56bf394e949d9ddc7b4ee489ed648b6da
Sergey Protasov <s.protasov@itransition.com>
2020-11-10 10:37:43 +0300

    Add parcel, use modules

---
+++ js/lib.js
@@ -0,0 +1,80 @@
+export const fadeOut = (el) => {
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+};
+
+export const fadeIn = (el, display) => {
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+};
+
+export const countTo = (el, duration = 10000) => {
+  el.started = true;
+
+  let start = null, step = null;
+  const to = parseInt(el.textContent, 10);
+  if (isNaN(to)) return;
+
+  el.textContent = 0;
+
+  (function count(timestamp) {
+    if (start) step = Math.trunc(to / (duration / (timestamp - start)));
+    if (!start) start = timestamp;
+    el.textContent = +el.textContent + (step || 1);
+
+    if (el.textContent < to) {
+      requestAnimationFrame(count);
+    } else {
+      el.textContent = to;
+    }
+  })();
+};
+
+
+export const nodeListToArray = (nodeList) => Array.prototype.slice.call(nodeList);
+
+export const mapToAsocciatedObject = (nodeList, getter) => nodeListToArray(nodeList)
+  .reduce((acc, node) => {
+      if (node.id) {
+        const el = getter(node.id);
+        if (!el) {
+          return acc;
+        }
+
+        acc[node.id] = el;
+      }
+
+      return acc;
+  },{});
+
+export const toggleClass = (flag, el, className) => flag
+  ? el.classList.add(className)
+  : el.classList.remove(className);
+
+export const setOpacity = (selector, opacity) => document.querySelectorAll(selector).forEach(el => {
+  if (opacity === 1) {
+    el.hidden = false;
+  }
+  el.style.opacity = opacity
+});
+
+
+export const inView = (target, options, action) => {
+  const observer = new IntersectionObserver(entries => entries.forEach(entry => action(entry)), options);
+  observer.observe(target);
+};
--- js/main.js
+++ js/main.js
@@ -1,135 +1,50 @@
-const fadeOut = (el) => {
-  el.style.opacity = 1;
-
-  (function fade() {
-    if ((el.style.opacity -= .1) < 0) {
-      el.style.display = "none";
-    } else {
-      requestAnimationFrame(fade);
-    }
-  })();
-};
-
-function fadeIn(el, display){
-  el.style.opacity = 0;
-  el.style.display = display || "block";
-
-  (function fade() {
-    var val = parseFloat(el.style.opacity);
-    if (!((val += .1) > 1)) {
-      el.style.opacity = val;
-      requestAnimationFrame(fade);
-    }
-  })();
-};
-
-const countTo = (el, duration = 10000) => {
-  el.started = true;
-
-  let start = null, step = null;
-  const to = parseInt(el.textContent, 10);
-  if (isNaN(to)) return;
-
-  el.textContent = 0;
-
-  (function count(timestamp) {
-    if (start) step = Math.trunc(to / (duration / (timestamp - start)));
-    if (!start) start = timestamp;
-    el.textContent = +el.textContent + (step || 1);
-
-    if (el.textContent < to) {
-      requestAnimationFrame(count);
-    } else {
-      el.textContent = to;
-    }
-  })();
-};
-
-
-const inView = (target, options, action) => {
-  const interSecObs = new IntersectionObserver(entries => {
-   entries.forEach(entry => {
-     action(entry);
-   });
-  }, options);
-  interSecObs.observe(target);
-};
-
-const nodeListToArray = (nodeList) => Array.prototype.slice.call(nodeList);
-const mapToAsocciatedObject = (nodeList, getter) => nodeListToArray(nodeList)
-  .reduce(
-    (acc, node) => {
-      if (node.id) {
-        const el = getter(node.id);
-        if (!el) {
-          return acc;
-        }
-
-        acc[node.id] = el;
-      }
-
-      return acc;
-    },
-    {}
-  );
-
-const toggleClass = (flag, el, className) => flag
-  ? el.classList.add(className)
-  : el.classList.remove(className);
+import { fadeOut, countTo, mapToAsocciatedObject, toggleClass, setOpacity, inView } from './lib.js'
 
 const scrollSpy = () => {
+  if (!("IntersectionObserver" in window)) {
+    return;
+  }
+
   const targets = document.querySelectorAll("header, .section"),
+    links = mapToAsocciatedObject(targets, id => document.querySelector(`#main-navbar a.nav-link[href='#${id}']`)),
     top = document.querySelector("#hero-area > div .contents"),
     navBar = document.querySelector(".scrolling-navbar"),
     backToTop = document.querySelector(".back-to-top"),
-    links = mapToAsocciatedObject(targets, (id) => document.querySelector(
-      `#main-navbar a.nav-link[href='#${id}']`
-    ));
+    counters = document.querySelectorAll('.counter');
 
-  // check if IntersectionObserver is supported
-  if ("IntersectionObserver" in window) {
-    const counters = document.querySelectorAll('.counter');
-    counters.forEach(c => inView(c, {threshold: 1}, entry => {
-      entry.isIntersecting && !entry.target.started && countTo(entry.target);
-    }));
+  counters.forEach(c => inView(c, { threshold: 1 }, entry => {
+    entry.isIntersecting && !entry.target.started && countTo(entry.target);
+  }));
 
-    inView(top, {threshold: 1}, (entry) => {
-      toggleClass(!entry.isIntersecting, navBar, "top-nav-collapse")
-      toggleClass(!entry.isIntersecting, backToTop, "opacity-on")
+  inView(top, { threshold: 1 }, (entry) => {
+    toggleClass(!entry.isIntersecting, navBar, "top-nav-collapse")
+    toggleClass(!entry.isIntersecting, backToTop, "opacity-on")
+  });
+
+  targets.forEach(target => {
+    inView(target, { rootMargin: "50px", threshold: 1 }, entry => {
+      const currentNav = links[entry.target.id];
+      if (!currentNav) {
+        return;
+      }
+      toggleClass(entry.isIntersecting, currentNav, "active");
     });
-
-    targets.forEach(target => {
-      inView(target, {rootMargin: "50px", threshold: 1}, entry => {
-        const currentNav = links[entry.target.id];
-        if (!currentNav) {
-          return;
-        }
-        toggleClass(entry.isIntersecting, currentNav, "active");
-      });
-    });
-  }
+  });
 }
-
-const setOpacity = (selector, opacity) => document.querySelectorAll(selector).forEach(el => {
-  if (opacity === 1) {
-    el.hidden = false;
-  }
-  el.style.opacity = opacity
-});
 
 const potfolio = () => {
   const filters = document.querySelectorAll('#portfolios a.filter');
   const all = "#portfolio > div";
 
-  document.querySelectorAll(all).forEach(c => c.addEventListener("transitionend",(event) => {
+  document.querySelectorAll(all).forEach(c => c.addEventListener("transitionend", (event) => {
     if (event.target.style.opacity === "0") {
       event.target.hidden = true;
     }
   }));
 
-  filters.forEach(f => f.addEventListener('click', function(event) {
+  filters.forEach(f => f.addEventListener('click', event => {
     const filterValue = event.target.getAttribute('data-filter');
-    filters.forEach(f => toggleClass(f.getAttribute('data-filter') === filterValue, f,'active'))
+    filters.forEach(f => toggleClass(f.getAttribute('data-filter') === filterValue, f, 'active'));
     if (filterValue === '*') {
       return setOpacity(all, 1);
     }
@@ -138,12 +53,15 @@ const potfolio = () => {
   }));
 }
 
+const gallery = () => {
+  const gallery = new Flickity('.gallery', { autoPlay: true, groupCells: true, wrapAround: true });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  const loader = document.querySelector('#loader');
+  gallery();
   potfolio();
-  const gallery = new Flickity('.gallery', {autoPlay:true, groupCells: true, wrapAround: true});
   scrollSpy();
+
+  const loader = document.querySelector('#loader');
   fadeOut(loader);
 });
-
-const app = Vue.createApp({});
