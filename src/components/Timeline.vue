79f93be7657314cb5331a27385ef13d86dc5a67a
Protasov, Sergey <s.protasov@itransition.com>
2022-01-13 17:08:03 +0300

    update packages

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -13,49 +13,23 @@ export default {
 ul {
   list-style-type: none;
   position: relative;
-  &:before {
+  &:before, &:after {
     content: ' ';
     background: black;
     display: inline-block;
     position: absolute;
+    z-index: 400;
+  }
+  &:before {
     left: 29px;
     width: 2px;
     height: 100%;
-    z-index: 400;
   }
   &:after {
-    content: ' ';
-    background: black;
-    display: inline-block;
-    position: absolute;
     bottom: 0px;
     left: 20px;
     width: 20px;
     height: 2px;
-    z-index: 400;
-  }
-
-  > li {
-    &:hover {
-      &:before {
-        background: black;
-      }
-    }
-    margin: 20px 0;
-    padding-left: 60px;
-    &:before {
-      transition: all 0.2s linear;
-      content: ' ';
-      background: white;
-      display: inline-block;
-      position: absolute;
-      border-radius: 50%;
-      border: 3px solid black;
-      left: 20px;
-      width: 20px;
-      height: 20px;
-      z-index: 400;
-    }
   }
 }
 </style>
--- src/components/TimelinePoint.vue
+++ src/components/TimelinePoint.vue
@@ -13,3 +13,28 @@ export default {
   props: ["title", "date"],
 };
 </script>
+
+<style lang="scss" scoped>
+li {
+  &:hover {
+    &:before {
+      background: black;
+    }
+  }
+  margin: 20px 0;
+  padding-left: 60px;
+  &:before {
+    transition: all 0.2s linear;
+    content: ' ';
+    background: white;
+    display: inline-block;
+    position: absolute;
+    border-radius: 50%;
+    border: 3px solid black;
+    left: 20px;
+    width: 20px;
+    height: 20px;
+    z-index: 400;
+  }
+}
+</style>
